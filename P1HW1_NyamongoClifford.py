211a146850f6a01704bc761c9d5e8c45ec57c9fb
nyamong5341 <99235134+nyamong5341@users.noreply.github.com>
2022-02-09 23:13:45 -0500

    Add files via upload

---
+++ P1HW1_NyamongoClifford.py
@@ -0,0 +1,17 @@
+print("Enter the first number\n")
+x = int(input())
+print("Enter the second number\n")
+y = int(input())
+
+#adding the two numbers
+z = x + y
+
+#multiplying the two numbers
+v = x * y
+
+print(x, end = '\n')
+print(y, end = '\n')
+print(z, end = '\n')
+print(v, end = '\n')
+
+
