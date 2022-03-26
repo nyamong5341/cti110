a1758b454652d92d9c50d77e339c7dceba222e94
nyamong5341 <99235134+nyamong5341@users.noreply.github.com>
2022-03-26 17:08:26 -0400

    Add files via upload
    Pizza Order using loops

--- P4HW1_NyamongoClifford.py
+++ P4HW1_NyamongoClifford.py
@@ -1,50 +1,55 @@
-#CTI-110
-# P4HW1 - Score List
-# NYamongo Clifford A
-#2022/03/21
+# CTI-110
+# P4HW2 - Pizza Order
+# Nyamongo Clifford
+# 2022/03/21
 #
-Students_No = int(input('Enter the number of scores you wish to enter?: '))
+
+Students_No = int(input('How many students do you want to order pizza for?: '))                #ASk user to enter number of students
+Pizza_number = float(input('Enter number of people per  pizza(1.5, 2 or 3): '))                #ask user to enter number of people per pizza
+while Pizza_number not in [1.5,2,3]:                                                           # program runs a while loop and only 1.5,2 and 3 are true
+
+    print('INVALID ENTRY!!!!!')
+    print('Should have entered 1.5, 2 or 3')
+    Pizza_number = float(input('Enter number of people per pizza again.(1.5, 2 or 3): '))       # program asks to enter again the number of people per pizza
+    print(Pizza_number)
+print('----Pizza order-------')
+
+No_pizza = (Pizza_number * Students_No) / 6           # Each pizza has 6 slices
+Price_beforetax = No_pizza * 5
+Tax = Price_beforetax + (0.06 * Price_beforetax)      #Culcutate Tax
+Price = Price_beforetax + Tax                         #Culculate price after tax
+print('Number of Students :'+str(Students_No))        # print number of students
+print('Pizzas Needed:' +str(round(No_pizza)))         #print number of pizza in whole numbers
+print('Price:$'+str(round(Price, 2)))                 #Print the totla price
+print('--------------------------')
+
+answer = input('Would you like to run the program again (Y for yes?) ')                            # asks the user if they want to run the program again
+while answer == "Y" or "y":
 
 
-scoreList=[]
-                               #Create a for loop that only validate the score between zero and 100
-for i in range(Students_No):
-    score = int(input("Enter Score #" + str(i + 1) + ": "))
+    print()
+    print()
+    print('---------------------------------------------')
+    Students_No = int(input('How many students do you want to order pizza for?: '))                #ASk user to enter number of students
+    Pizza_number = float(input('Enter number of people per  pizza(1.5, 2 or 3): '))                #ask user to enter number of people per pizza
+while Pizza_number not in [1.5,2,3]:                                                           # program runs a while loop and only 1.5,2 and 3 are true
 
-    while score < 0 or score > 100:
-        print('INVALID ENTRY!!!')
-        print('Should have entered between 0 and 100' )
-        score = int(input('Score should be between 0 and 100?: '))
+    print('INVALID ENTRY!!!!!')
+    print('Should have entered 1.5, 2 or 3')
+    Pizza_number = float(input('Enter number of people per pizza again.(1.5, 2 or 3): '))       # program asks to enter again the number of people per pizza
+    print(Pizza_number)
+print('----Pizza order-------')
 
-    scoreList.append(score)
+No_pizza = (Pizza_number * Students_No) / 6           # Each pizza has 6 slices
+Price_beforetax = No_pizza * 5
+Tax = Price_beforetax + (0.06 * Price_beforetax)      #Culcutate Tax
+Price = Price_beforetax + Tax                         #Culculate price after tax
+print('Number of Students :'+str(Students_No))        # print number of students
+print('Pizzas Needed:' +str(round(No_pizza)))         #print number of pizza in whole numbers
+print('Price:$'+str(round(Price, 2)))                 #Print the totla price
+print('--------------------------')
 
-lowest_Score = min(scoreList)
-scoreList.remove(lowest_Score)
-average = sum(scoreList) / (Students_No - 1)
-
-
-print(scoreList)                             #output score list
-print('Lowest score: ' + str(lowest_Score))   #output lowest score
-print('modified list: ' + str(scoreList))     # output lowest score after removing the lowest score
-print('Scores average: ' + str(average))
-
-                                                     # system uses 10-point grading scale
-if average>=90 and average<=100:
-    print('Grade = A')
-elif average>=80 and average<90:
-    print('Grade = B')
-elif average>=70 and average<80:
-    print('Grade = C')
-elif average>=60 and average<70:
-    print('Grade = D')
-elif average>=50 and average<60:
-    print('Grade = E')
-else:
-    print('Grade = F')
-
-
-
-
+answer = input('Would you like to run the program again (Y for yes)? ')
 
 
 
